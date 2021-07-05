df0321a0cd2f5c6f558552622b7df6dc123a7040
Guilherme2k3tech <guilhermemoraeskoagoski.contato@gmail.com>
2021-07-05 11:16:27 -0300

    Clone-Twitter-0.1

---
+++ src/components/Feed/Index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Feed: React.FC = () => {
+  return (
+        
+
+
+
+  );
+}
+
+export default Feed;
---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+
+
+import { Container, Banner, Avatar, ProfileData,LocationIcon, CakeIcon, Fallowage, EditButton} from './styles';
+
+const ProfilePage: React.FC = () => {
+  return (
+      <Container>
+          
+          <Banner>
+            <Avatar>
+
+            </Avatar>
+          </Banner>
+
+          <ProfileData>
+           <EditButton outlined>Editar Perfil</EditButton>
+          <h1>Ermes</h1>
+          <h2>@oErmes</h2>
+
+            <p>
+                Content Creator at <a href='https://www.youtube.com/channel/UCc5h6tUGA6LfLma9_WZh8Pg'>YouTube</a>
+            </p>
+
+            <ul>
+              <li>
+                <LocationIcon/>
+                    Joinville, SC.
+              </li>
+              <li>
+                <CakeIcon/>
+                    28 de Setembro de 2003
+              </li>
+            </ul>
+
+            <Fallowage>
+              <span>Seguindo <strong> 3</strong></span>
+              <span>
+                <strong>45 </strong> Seguidores
+                </span>
+
+            </Fallowage>
+          </ProfileData>
+
+
+      </Container>
+  );
+}
+
+export default ProfilePage;
